e77561e7b1b5e31c7756aa4b59599c508d749b2f
ray rene vega huaman <90875621+rayvega20@users.noreply.github.com>
2022-09-30 14:06:08 -0500

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,54 @@
+<?php
+   //Reseteamos variables a 0.
+   $nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+   if (isset($_POST['submit'])) {
+      //Obtenemos valores input formulario
+      $nombre = $_POST['nombre'];
+      $email = $_POST['email'];
+      $subject = $_POST['subject'];   
+      $mensaje = $_POST['mensaje'];
+      $para = 'rave@ray.corpipsa.com';
+
+      //Creamos cabecera.
+      $headers = 'From' . " " . $email . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $nombre;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Email: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Asunto: " . $subject;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $mensaje;
+      $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+         echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+         echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
+   }
+   echo "
+   
+   
+   
+   
+   
+   
+            POR FAVOR VUELVA A LA PAGINA ANTERIOR 
+            
+            
+            
+            
+            
+            
+            
+            
+            "
+?>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mail Form</title>
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+  <form id="main-contact-form" class="contact-form" name="contact-form" action="correo.php" method="POST">
+      <div class="col-sm-5 col-sm-offset-1">
+          <div class="form-group">
+             <label>Nombre :</label>
+             <input type="text" id="nombre" name="nombre" class="form-control" required="required">
+           </div>
+
+           <div class="form-group">
+              <label>Email :</label>
+              <input type="email" id="email" name="email" class="form-control" required="required">
+           </div> 
+
+           <div class="form-group">
+               <label>Asunto :</label>
+               <input type="text" id="subject" name="subject" class="form-control" required="required">
+           </div>
+
+           <div class="form-group">
+                <label>Mensaje :</label>
+                <textarea name="mensaje" id="mensaje" required="required" class="form-control" rows="8"></textarea>
+           </div>                        
+           
+           <div>
+    
+     <center> 
+
+      <div class="g-recaptcha"  data-sitekey="6LfpkDIiAAAAAOH1ueZeQr3Jwr18toNUynIQ0psz" required=""></div>
+
+     </center>
+
+      <script>
+
+          window.onload = function() {
+          var $recaptcha = document.querySelector('#g-recaptcha-response');
+
+          if($recaptcha) {
+              $recaptcha.setAttribute("required", "required");
+          }};
+
+      </script>
+
+    </div>
+       
+           
+
+           <div class="form-group">
+                <input type="submit" id="submit" name="submit" class="btn btn-primary btn-lg" value="Enviar" />
+           </div>                      
+      </div> 
+      <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</form>
+
+</body>
+</html>
